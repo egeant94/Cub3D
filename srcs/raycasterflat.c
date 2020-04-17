a49f6a9e2262702a4b29e5cf07916be34e179a99
Geant Enzo 42LINUX vm <egeant@student.42.fr>
2020-04-17 15:03:55 +0200

    pas sur pour le fisheye, j'essaye dimplementer un mouvement

--- srcs/raycasterflat.c
+++ srcs/raycasterflat.c
@@ -15,15 +15,14 @@
 #define screenHeight 1000
 #define map_max 6
 #define FOV 90
-#define WALL_HEIGHT 0.3
+#define WALL_HEIGHT 1
 typedef struct s_camera
 {
 	int player_x;
 	int player_y;
 	float player_dx;
 	float player_dy;
-	float dir_x;
-	float dir_y;
+	float h_o_v;
 	float beta_ang;
 	float theta_ang;
 	float x_intercept;
@@ -43,6 +42,8 @@ typedef struct s_mlx_data
 	int bits_per_pixel;
 	int line_length;
 	int endian;
+	int worldMap[6][6];
+	t_camera *cam;
 } t_mlx_data;
 
 typedef struct s_coordinates
@@ -53,13 +54,12 @@ typedef struct s_coordinates
 
 void init_camera(t_camera *cam)
 {
-	cam->player_x = 1;
+	cam->player_x = 4;
 	cam->player_y = 3;
 	cam->player_dx = 0.5;
-	cam->player_dy = 0;
-	cam->dir_x = 0;
-	cam->dir_y = -1;
-	cam->beta_ang = 160;
+	cam->player_dy = 0.5;
+	cam->h_o_v = 0;
+	cam->beta_ang = 210;
 	cam->theta_ang = 0;
 	cam->x_intercept = 0;
 	cam->y_intercept = 0;
@@ -183,39 +183,48 @@ float min_ray_dist(t_camera *cam, int worldMap[6][6], float angle)
 		y_intercept.x += cam->tile_step_y / tan(rad_angle);
 		y_intercept.y += cam->tile_step_y;
 	}
-	if (worldMap[0][0] == 1)
-		(void)worldMap;
 	hor_dist = sqrt(pow(x_intercept.x - (cam->player_x + cam->player_dx), 2) + pow(x_intercept.y - (cam->player_y + cam->player_dy), 2));
 	ver_dist = sqrt(pow(y_intercept.x - (cam->player_x + cam->player_dx), 2) + pow(y_intercept.y - (cam->player_y + cam->player_dy), 2));
 	if (hor_dist < ver_dist)
 	{
-		return (hor_dist);
-		printf("%f, %f, %f, %f |", angle, angle_to_quadrant(&cam->beta_ang, cam), hor_dist, fabs(quadrant_to_angle(&angle, cam) - cam->beta_ang));
+		cam->h_o_v = 1;
+		return (hor_dist * cos(fabs(quadrant_to_angle(&angle, cam) - cam->beta_ang) * M_PI / 180));
 		// return (hor_dist * cos((quadrant_to_angle(&angle, cam) * M_PI / 180.00) - (cam->beta_ang * M_PI / 180.0)));
 		// return ((fabs(x_intercept.x - (cam->player_x + cam->player_dx))) * cos(angle_to_quadrant(&cam->beta_ang, cam) * M_PI / 180.0)
 				// + (fabs(x_intercept.y - (cam->player_y + cam->player_dy))) * sin(angle_to_quadrant(&cam->beta_ang, cam) * M_PI / 180.0));
 	}
 	else
 	{
-		return(ver_dist);
-		printf("%f, %f, %f, %f |", angle, angle_to_quadrant(&cam->beta_ang, cam), ver_dist, fabs(quadrant_to_angle(&angle, cam) - cam->beta_ang));
+		cam->h_o_v = 2;
+		return(ver_dist * cos(fabs(quadrant_to_angle(&angle, cam) - cam->beta_ang) * M_PI / 180));
 		// return (ver_dist * cos((quadrant_to_angle(&angle, cam) * M_PI / 180.00) - (cam->beta_ang * M_PI / 180.0)));
 		// return ((fabs(y_intercept.x - (cam->player_x + cam->player_dx))) * cos(angle_to_quadrant(&cam->beta_ang, cam) * M_PI / 180.0)
 				// + (fabs(y_intercept.y - (cam->player_y + cam->player_dy))) * sin(angle_to_quadrant(&cam->beta_ang, cam) * M_PI / 180.0));
 	}
 }
 
-void	print_vertical_line(t_mlx_data *mlx, int x, float dist)
+void	print_vertical_line(t_mlx_data *mlx, int x, float dist, t_camera cam)
 {
 	float		y;
+	float		wall;
+	int			color;
 
+	if (cam.h_o_v == 1 && cam.tile_step_x == 1)
+		color = 0x00FF0000;
+	if (cam.h_o_v == 1 && cam.tile_step_x == -1)
+		color = 0x0000FF00;
+	if (cam.h_o_v == 2 && cam.tile_step_y == -1)
+		color = 0x000000FF;
+	if (cam.h_o_v == 2 && cam.tile_step_y == 1)
+		color = 0x00FFFFFF;
 	y = 0;
+	wall = (float)WALL_HEIGHT / dist;
 	while (y < screenHeight / 2)
 	{
-		if ((y / (screenHeight / 2)) * dist <= WALL_HEIGHT)
+		if ((y / (screenHeight / 2)) < wall)
 		{
-			my_mlx_pixel_put(mlx, x, screenHeight / 2 + y, 0xFFFFFF00);
-			my_mlx_pixel_put(mlx, x, screenHeight / 2 - y, 0xFFFFFF00);
+			my_mlx_pixel_put(mlx, x, screenHeight / 2 + y, color);
+			my_mlx_pixel_put(mlx, x, screenHeight / 2 - y, color);
 		}
 		y++;
 	}
@@ -235,8 +244,7 @@ void calculate_first_frame(t_mlx_data *mlx, t_camera *cam, int worldMap[6][6])
 	{
 		quadrant_theta = angle_to_quadrant(&cam->theta_ang, cam);
 		float dist = min_ray_dist(cam, worldMap, quadrant_theta);
-		print_vertical_line(mlx, column, dist);
-		printf("%6.2f, %f\n", cam->theta_ang, dist); 
+		print_vertical_line(mlx, column, dist, *cam);
 		// min_ray_dist(cam, worldMap, quadrant_theta);
 		cam->theta_ang -= ((float)FOV / ((float)screenWidth - 1.0));
 		column++;
@@ -244,22 +252,30 @@ void calculate_first_frame(t_mlx_data *mlx, t_camera *cam, int worldMap[6][6])
 	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
 }
 
+int	move(int keycode, t_mlx_data *mlx)
+{
+	(void)keycode;
+	mlx->cam->beta_ang++;
+	calculate_first_frame(mlx, mlx->cam, *mlx->worldMap);
+}
+
 int main(void)
 {
 	t_camera cam;
 	t_mlx_data mlx;
-	int worldMap[6][6] =
-		{
+	mlx.worldMap[6][6] = {
 			{1, 1, 1, 1, 1, 1},
 			{1, 0, 0, 0, 0, 1},
 			{1, 0, 1, 0, 0, 1},
-			{1, 0, 0, 0, 3, 1},
+			{1, 0, 0, 0, 0, 1},
 			{1, 0, 0, 0, 0, 1},
 			{1, 1, 1, 1, 1, 1}};
 
 	init_camera(&cam);
 	mlx.mlx = mlx_init();
 	mlx.win = mlx_new_window(mlx.mlx, screenWidth, screenHeight, "raycasterflat");
-	calculate_first_frame(&mlx, &cam, worldMap);
+	calculate_first_frame(&mlx, &cam, mlx.worldMap);
+	mlx.cam = &cam;
+	mlx_key_hook(mlx.win, move, &mlx);
 	mlx_loop(mlx.mlx);
 }
---
+++ srcs/test.c
@@ -0,0 +1,90 @@
+#include "../includes/raycasterflat.h"
+#define screenWidth 1000
+#define screenHeight 1000
+#define map_max 6
+#define FOV 90
+#define WALL_HEIGHT 0.5
+typedef struct s_camera
+{
+	int player_x;
+	int player_y;
+	float player_dx;
+	float player_dy;
+	float dir_x;
+	float dir_y;
+	float beta_ang;
+	float theta_ang;
+	float x_intercept;
+	float y_intercept;
+	float x_step;
+	float y_step;
+	int tile_step_x;
+	int tile_step_y;
+} t_camera;
+
+typedef struct s_mlx_data
+{
+	void *mlx;
+	void *win;
+	void *img;
+	void *addr;
+	int bits_per_pixel;
+	int line_length;
+	int endian;
+} t_mlx_data;
+
+typedef struct s_coordinates
+{
+	float x;
+	float y;
+} t_coordinates;
+
+void my_mlx_pixel_put(t_mlx_data *mlx, int x, int y, int color)
+{
+	char *dst;
+
+	dst = mlx->addr + (y * mlx->line_length + x * (mlx->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+void	print_vertical_line(t_mlx_data *mlx, int x, float dist)
+{
+	float		y;
+	float		wall;
+
+	y = 0;
+	wall = (float)WALL_HEIGHT / dist;
+	while (y < screenHeight / 2)
+	{
+		if ((y / (screenHeight / 2)) < wall)
+		{
+			my_mlx_pixel_put(mlx, x, screenHeight / 2 + y, 0xFFFFFF00);
+			my_mlx_pixel_put(mlx, x, screenHeight / 2 - y, 0xFFFFFF00);
+		}
+		y++;
+	}
+}
+
+int	main(void)
+{
+	t_mlx_data mlx;
+
+	mlx.mlx = mlx_init();
+	mlx.win = mlx_new_window(mlx.mlx, screenWidth, screenHeight, "raycasterflat");
+	mlx.img = mlx_new_image(mlx.mlx, screenWidth, screenHeight);
+	mlx.addr = mlx_get_data_addr(mlx.img, &mlx.bits_per_pixel,
+								  &mlx.line_length, &mlx.endian);
+	int	column = 0;
+	float ray_dist = 0.4;
+	float previous = ray_dist;
+	while (column < screenWidth)
+	{
+		print_vertical_line(&mlx, column, ray_dist);
+		ray_dist += 0.4 / (float)screenWidth;
+		printf("%f\n", ray_dist);
+		previous = ray_dist;
+		column++;
+	}
+	mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img, 0, 0);
+	mlx_loop(mlx.mlx);
+}
